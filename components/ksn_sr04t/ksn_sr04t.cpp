3f5e7f22d7041ed5124374e9acc347f058e0a104
Kevin Regimbal <kregimbal@gmail.com>
2025-02-07 18:08:18 -0700

    debugging

--- components/ksn_sr04t/ksn_sr04t.cpp
+++ components/ksn_sr04t/ksn_sr04t.cpp
@@ -45,12 +45,12 @@ void Ksnsr04tComponent::check_buffer_() {
 
   if (this->buffer_[3] == checksum) {
     uint16_t distance = encode_uint16(this->buffer_[1], this->buffer_[2]);
-    if (distance > 25) {
+    if (distance > 200) {
       float centimeters = distance / 10.0f;
       ESP_LOGV(TAG, "Distance from sensor: %umm, %.3fm", distance, meters);
       this->publish_state(centimeters);
     } else {
-      ESP_LOGW(TAG, "Invalid data read from sensor: %s", format_hex_pretty(this->buffer_).c_str());
+      ESP_LOGW(TAG, "Invalid distance (%i) data read from sensor: %s", distance, format_hex_pretty(this->buffer_).c_str());
     }
   } else {
     ESP_LOGW(TAG, "checksum failed: %02x != %02x", checksum, this->buffer_[3]);
